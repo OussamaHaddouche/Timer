0576204005007aa2d9221e15bb5cba61f524de90
OussamaHaddouche <oussama1haddouche2@gmail.com>
2020-03-30 23:04:26 +0100

    finished timer working

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton
-        this.pauseButton = pauseButton
-
-        this.startButton.addEventListener("click", this.start)
-        this.pauseButton.addEventListener("click", this.pause)
-    }
-
-    start = () => {
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);  
-    } 
-
-    pause = () => {
-        clearInterval(this.interval)
-    }
-
-    tick = () => {
-        durationInput.value-= 1;
-    }
-}
-
-
 const durationInput=document.querySelector("#duration");
 const startButton=document.querySelector("#start");
 const pauseButton=document.querySelector("#pause");
+const circle = document.querySelector("circle")
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+let totalDuration;
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(timeRemaining) {
+    totalDuration = timeRemaining;
+    },
+    onTick(timeRemaining){
+    circle.setAttribute("stroke-dashoffset", perimeter * timeRemaining/totalDuration-perimeter);
+    },
+    onComplete(){
+        console.log("Timer finished")
+    }
+});
